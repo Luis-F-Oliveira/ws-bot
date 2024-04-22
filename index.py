b670283bbb0a220231006026cc7766b758d6bb32
Luis-F-Oliveira <luis.f.oliveira505141@outlook.com>
2024-04-22 17:18:33 -0400

    upd python

--- index.py
+++ index.py
@@ -1,14 +1,18 @@
 import json
 import subprocess
+import os
 
 email = input('Qual seu email? ')
 password = input('Qual sua senha? ')
 
 data = { 'email': email, 'password': password }
 
+if not os.path.exists('./json'):
+    os.makedirs('./json')
+
 with open('./json/account.json', 'w') as file:
     json.dump(data, file)
 
-subprocess.run(["C:\\Program Files\\nodejs\\npm.cmd", "install"])
+subprocess.run(["C:\\Program Files\\nodejs\\npm.cmd", "install", "@types/node"])
 subprocess.run(["C:\\Program Files\\nodejs\\npx.cmd", "tsc"])
-subprocess.run(["C:\\Program Files\\nodejs\\npm.cmd", "run", "start"])
+subprocess.run(["C:\\Program Files\\nodejs\\npm.cmd", "run", "dev"])
